c23b318e3ca11052225af55f716a8902d83b125d
Kasia <120297090+kasiakolodziejska@users.noreply.github.com>
2023-12-04 11:32:12 +0100

    Adding files

---
+++ PythonKasia/intro.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# komentarz
+
+print (2 + 3)
+
+# %%
+
+2 + 3
+
+10 / 3
+
+2 + 3
+
+# %%
+
+10 / 3
+
+# %%
+10 // 3
+
+# %%
+
+2 ** 5
+
+# %%
+10 % 3
+12 % 5
+
+# %%
+(10 - 2 ** 3) * 10
+
+# %%
+a = 10
+b = 20
+
+c = a * b
+
+print (c)
+
+'love python'
+
+# %%
+
+"it\'s the best"
+
+# %% 
+
+print ('Python 3.7')
+print ('Python\n3.7')
+
+# %%
+print("""Python
+3.7""")
+
+# %%
+
+print ('\tPython')
+print('\t\t\tPython')
+
+# %%
+print (""""
+Instruckcja uruchamiania pliku przyklad.py
+-- file [nazwa pliku]
+       zapisuje output do pliku
+-- quiet
+wycisza logi w konsoli
+Koniec.  
+       """)
+       
+# %% 
+text = 'I love Python. '
+
+print (text * 3)
+
+print('hau...' * 8)
+
+# %%
+'Python'
+
+print("Py" "thon")
+
+# %%
+name = 'python'
+
+print(name + ' \nchuj')
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Python course
- course materials, practise
+# python kurs
+
+Repozytorium jest częścią kursu [Programowanie w języku Python - od A do Z](https://www.udemy.com/programowanie-w-jezyku-python/?couponCode=BONUSCODE) dostępnego na platformie Udemy.com
